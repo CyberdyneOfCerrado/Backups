0d3884be60e52de0a2a25788805a1be49722d133
allysom <allysom@Naltilus>
2014-03-22 02:58:05 -0300

    Configuração

--- src/teste/Principal.java
+++ src/teste/Principal.java
@@ -15,20 +15,5 @@ import GUI.SingleGuiMain;
 public class Principal {
 	public static void main (String [] args ){
 		new SingleGuiMain();
-		
-		HistoricoBackups hb = new HistoricoBackups();
-		
-		IteradorBackups ib = hb.obterHistorico();
-		
-		while(ib.hasNext()){
-			Backup b = ib.next();
-			
-			IteradorVersoes iv = b.resgatarVersoes();
-			while(iv.hasNext()){
-				Versao v = iv.next();
-				System.out.println("FK: "+b.primaryKey+" id: "+ v.primaryKey);
-			}
-		}
-		//allysom mudei seu carregamento de banco pro pacote Banco qualquer coisa fala comigo ok
 	}
 }
